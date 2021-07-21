8160997fe910f059424d27512d0b3f45f9ad194c
Mustafa Taylan Ulun <taylan.ulun@outlook.com>
2021-07-22 02:21:49 +0300

    opening page

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY > 0);
+    header.classList.toggle('sticky', window.scrollY > 0);
 });
 
 
-function toggleMenu(){
+function toggleMenu() {
     const menuToggle = document.querySelector('.menuToggle');
     const navigation = document.querySelector('.navigation');
     menuToggle.classList.toggle('active');
